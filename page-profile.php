daee40d147d2b44e075b406c5481a1d6a8cdc835
note-illust <45662744+note-illust@users.noreply.github.com>
2018-12-06 23:30:17 +0900

    Create page-profile.php

---
+++ page-profile.php
@@ -0,0 +1,29 @@
+<?php
+get_header(); ?>
+<main role="main">
+    <article>
+        <section>
+            <div class="inner">
+                <div class="cf">
+                    <div class="left profile_img">
+                        <img src="<?php echo get_template_directory_uri(); ?>/static/img/common/miku_dropheart_note.jpg" alt="Note Illustration">
+                    </div>
+                    <div class="left profile_text">
+                        ＜Profile＞ <br>
+                         <br>
+                        Note／のーと <br>
+                        <br>
+                        関東在住のクリエイター <br>
+                        2018年3月頃から絵師としての活動を始める <br>
+                        <br>
+                        ■活動：ボカロPVのイラスト制作・オリジナル動画制作・音声ドラマの企画などなど <br>
+                        ■制作環境：Clip studio paint・After Effects <br>
+                        ■twitter：<a href="https://twitter.com/note_illust" target="_blank">@note_illust</a><br>
+                        ■ニコ動マイリス：<a href="https://www.nicovideo.jp/mylist/62143728" target="_blank">nicovideo.jp/mylist/62143728</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </article>
+</main>
+<?php get_footer();
