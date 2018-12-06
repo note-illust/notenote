5622a43cc5c841909c70c3915821bbdcccbecc50
note-illust <45662744+note-illust@users.noreply.github.com>
2018-12-06 23:28:16 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+    ©2018 notenote.com
+</footer>
+<?php wp_footer(); ?>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/flickity/2.1.2/flickity.pkgd.min.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/static/js/javascript.js"></script>
+</body>
+</html>
